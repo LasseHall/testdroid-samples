05336544199b67239920ac57628c4dc09f2933b8
Lasse Häll <lasse.hall@bitbar.com>
2017-03-02 14:23:34 +0200

    add -f to rm in createZip.sh to avoid error messages

--- image-recognition/createAndroidZip.sh
+++ image-recognition/createAndroidZip.sh
@@ -7,7 +7,7 @@ OUT_ZIP="android-test.zip"
 
 cp run-tests_android.sh run-tests.sh
 if [ -f "${OUT_ZIP}" ]; then
-    rm "${OUT_ZIP}"
+    rm -f "${OUT_ZIP}"
 fi
 STASH=$(git stash create)
 git archive -o "${OUT_ZIP}" --format=zip $STASH pom.xml run-tests.sh src lib/linux queryimages
--- image-recognition/createiOSZip.sh
+++ image-recognition/createiOSZip.sh
@@ -7,7 +7,7 @@ OUT_ZIP="ios-test.zip"
 
 cp run-tests_ios.sh run-tests.sh
 if [ -f "${OUT_ZIP}" ]; then
-    rm "${OUT_ZIP}"
+    rm -f "${OUT_ZIP}"
 fi
 STASH=$(git stash create)
 git archive -o "${OUT_ZIP}" --format=zip ${STASH} pom.xml run-tests.sh src lib/mac queryimages
